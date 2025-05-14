f911e68ba391098c7f62e3ea6f60d884d9393994
saitejv23 <saiteja.v@seanergydigital.com>
2025-05-14 17:39:04 +0530

    Modified Code files

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,9 @@
 from calculator import add, subtract
 
+
 def test_add():
-    assert add(3, 4) == 7
+    assert add(2, 3) == 5
+
 
 def test_subtract():
-    assert subtract(10, 5) == 5
+    assert subtract(5, 2) == 3
